06d0ef34849a24523486ede96f1e3e159d6e3c12
K-KY <kyuyoungk@naver.com>
2023-12-06 16:41:59 +0900

    Refactor(InputView) : winNumber메서드가 WinLotto타입을 리턴하도록 변경

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -17,14 +17,14 @@ public class InputView {
         return new Money(Integer.parseInt(input));
     }
 
-    public static Lotto winNumber() {
+    public static WinLotto winNumber() {
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine().replace(" ", "");
 
         List<String> inputNumbers = List.of(input.split(","));
         inputNumbers.forEach(NumberValidator::validate);
         List<Integer> winNumbers = inputNumbers.stream().mapToInt(Integer::parseInt).boxed().toList();
-        return new Lotto(winNumbers);
+        return new WinLotto(new Lotto(winNumbers));
     }
 
     public static BonusNumber bonusNumber(WinLotto winLotto) {
