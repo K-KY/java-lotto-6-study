6fa798e8dbf23b7fbfde90c20e2ff8900c2edd39
K-KY <kyuyoungk@naver.com>
2023-12-06 15:54:43 +0900

    Feat(InputView) : 로또 당첨 번호 입력 메서드

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import lotto.Lotto;
 import lotto.model.Money;
 import lotto.utils.validator.NumberValidator;
 
@@ -11,5 +13,15 @@ public class InputView {
         String input = Console.readLine();
         NumberValidator.validate(input);
         return new Money(Integer.parseInt(input));
+    }
+
+    public static Lotto winNumber() {
+        System.out.println("당첨 번호를 입력해 주세요.");
+        String input = Console.readLine().replace(" ", "");
+
+        List<String> inputNumbers = List.of(input.split(","));
+        inputNumbers.forEach(NumberValidator::validate);
+        List<Integer> winNumbers = inputNumbers.stream().mapToInt(Integer::parseInt).boxed().toList();
+        return new Lotto(winNumbers);
     }
 }
