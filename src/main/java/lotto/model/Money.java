c6648e3d3c0ca638f08de0a281dfc414e4043f01
K-KY <kyuyoungk@naver.com>
2023-12-06 15:10:35 +0900

    Feat(Money) : 생성자 호출과 동시에 검증

--- src/main/java/lotto/model/Money.java
+++ src/main/java/lotto/model/Money.java
@@ -1,13 +1,18 @@
 package lotto.model;
 
+import lotto.utils.validator.MoneyValidator;
+
 public class Money {
     private final int money;
 
     public Money(int money) {
         this.money = money;
+        validate();
     }
 
     private void validate() {
-
+        MoneyValidator.isUnitFollowed(money);
+        MoneyValidator.isMinAmount(money);
+        MoneyValidator.isMaxAmount(money);
     }
 }
