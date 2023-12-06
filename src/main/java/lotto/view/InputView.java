3b3c280bd2cc6699f5efb06af3a7176db6cd9618
K-KY <kyuyoungk@naver.com>
2023-12-06 16:31:47 +0900

    Feat(InputView) : 보너스 번호 입력 메서드 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -3,7 +3,9 @@ package lotto.view;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 import lotto.Lotto;
+import lotto.model.BonusNumber;
 import lotto.model.Money;
+import lotto.model.WinLotto;
 import lotto.utils.validator.NumberValidator;
 
 public class InputView {
@@ -23,5 +25,12 @@ public class InputView {
         inputNumbers.forEach(NumberValidator::validate);
         List<Integer> winNumbers = inputNumbers.stream().mapToInt(Integer::parseInt).boxed().toList();
         return new Lotto(winNumbers);
+    }
+
+    public static BonusNumber bonusNumber(WinLotto winLotto) {
+        System.out.println("보너스 번호를 입력해 주세요.");
+        String input = Console.readLine();
+        NumberValidator.validate(input);
+        return new BonusNumber(Integer.parseInt(input), winLotto);
     }
 }
